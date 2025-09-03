a640b75b7d09d5677256277bf9215eb541f6dfea
Shubham <byshubham6@gmail.com>
2025-09-03 16:18:54 +0530

    Fix Docker group permission issue

--- vars/installDocker.groovy
+++ vars/installDocker.groovy
@@ -3,7 +3,9 @@ def call() {
         curl -fsSL https://get.docker.com -o get-docker.sh
         sudo sh get-docker.sh
         sudo usermod -aG docker $USER
-        docker --version
+        newgrp docker << EOF
+docker --version
+EOF
     '''
     echo "Docker installed successfully"
 }
